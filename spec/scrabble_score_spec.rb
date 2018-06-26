4a6d1881346199352db991ca581bb9b0439f2e97
Nate Cottle and Will Swanson <nate@>
2018-06-26 09:12:36 -0700

    add passing tests

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -2,7 +2,13 @@ require('rspec')
 require('scrabble_score')
 
 describe('#scrabble_score') do
-  it('accepts input') do
-    expect(scrabble_score("frogs")).to(eq(true))
+  it('accepts input of frog') do
+    expect(scrabble_score("frogs")).to(eq(9))
+  end
+  it('accepts input of letter q and returns score of 10') do
+    expect(scrabble_score("q")).to(eq(10))
+  end
+  it('parse word into individual letters and parse score') do
+    expect(scrabble_score("frogs")).to(eq(9))
   end
 end
